5f9bc2c4d7a35063e371127a11934bb9f782dd7c
CamKirk <camden.kirkland@gmail.com>
2019-07-30 17:26:04 -0500

    finish wk2 day1

---
+++ class/02-Python/1/Activities/09-Stu_Loops/Solved/cheer.py
@@ -0,0 +1,18 @@
+# Create a variable named cheer
+cheer = "Python"
+
+# Below strings can be used to add fun
+cheer_symbol = "*\O/*"
+cheer_symbol_2 = "ヘ( ^o^)ノ＼(^_^ )"
+
+# Loop through string
+for letter in cheer:
+    #Print each letter with a cheer
+    print("Give me a " + letter + "!")
+    print(letter + "!")
+
+# Print excitement to screen
+print("\nWhat does that spell?!")
+print(cheer + "!\nWoohoo! Go " + cheer + "!")
+print(cheer_symbol * 3)
+print(cheer_symbol_2)
